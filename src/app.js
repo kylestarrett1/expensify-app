ba025133d66565f9fc96ebed450943feba1b9b97
Kyle Starrett <kylestarrett1@gmail.com>
2022-11-12 17:23:37 -0800

    Connected Redux with React. Added state changes to Expense list.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("water"));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter("rent"));
+}, 3000);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
 const root = createRoot(document.getElementById("app"));
-root.render(<AppRouter />);
+root.render(jsx);
--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,48 +1,73 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }, action) => {
+// Action generators - functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy,
+});
+
+// setCount
+const setCount = ({ count }) => ({
+  type: "SET",
+  count,
+});
+
+const resetCount = () => ({
+  type: "RESET",
+});
+
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + action.incrementBy,
       };
 
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
 
+    case "SET":
+      return {
+        count: action.count,
+      };
     case "RESET":
       return {
-        count: (state.count = 0),
+        count: 0,
       };
 
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5,
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-unsubscribe();
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: "INCREMENT",
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: "RESET",
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: "DECREMENT",
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(setCount({ count: -100 }));
