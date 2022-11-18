900042e245bb0cdbc56cb4ffe4c4f8e142874a63
Kyle Starrett <kylestarrett1@gmail.com>
2022-11-17 17:52:44 -0800

    Added expenses from expenses selector

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,12 @@
-// Export a stateless functional component
-// description, amount, createdAt
 import React from "react";
-import { connect } from "react-redux";
 
-const expenseListItem = (props) => {
-  //   console.log(props.expenses[1].description);
-  console.log(props.expenses);
-};
+const ExpenseListItem = ({ description, amount, createdAt }) => (
+  <div>
+    <h3>{description}</h3>
+    <p>
+      {amount} - {createdAt}
+    </p>
+  </div>
+);
 
-const mapStateToProps = (state) => {
-  return {
-    expenses: state.expenses,
-    filters: state.filters,
-  };
-};
-
-export default connect(mapStateToProps)(expenseListItem);
+export default ExpenseListItem;
