3bc6707dc784bc50b55d8395df8fe38c2e128323
Kyle Starrett <kylestarrett1@gmail.com>
2022-11-19 11:52:13 -0800

    Wired up AddExpensePage to accept expense, then added each to Redux store.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import ExpenseForm from "./ExpenseForm";
+import { connect } from "react-redux";
+import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = () => (
-  <div>
-    <p>This is from my ADD expense page!</p>
-  </div>
-);
+const AddExpensePage = (props) => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm
+        onSubmit={(expense) => {
+          props.dispatch(addExpense(expense));
+          navigate("/");
+        }}
+      />
+    </div>
+  );
+};
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
